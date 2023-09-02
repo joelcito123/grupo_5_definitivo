231a20c5f8d4eca6acc15bd295d528876c0876ed
ramox-mq <daniel.rsi6oa@gmail.com>
2023-09-01 21:39:28 -0300

    creé los formularios para creación y edición de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,12 @@ const productController = {
     detalle: (req, res) => {
         res.render('productDetail')
     },
+    edicion: (req, res) => {
+        res.render('formulario-edicion-producto');
+    },
+    creacion: (req, res) => {
+        res.render('formulario-creacion-producto');
+    },
 }
 
 module.exports = productController;
