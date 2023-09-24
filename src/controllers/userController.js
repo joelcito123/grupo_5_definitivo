897454ab15e4e90eea0d070ffd76ca4946b1102a
ramox-mq <daniel.rsi6oa@gmail.com>
2023-09-24 15:35:05 -0300

    implemente el registro de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcryptjs')
 
 const usersFilePath = path.join(__dirname, '../data/usersData.json');
 const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -12,17 +13,33 @@ const userController = {
         res.render('register')
     },
     loged: (req, res) => {
-        res.send('logeado');
+        let usuario;
+        if (usuarios == "") {
+            usuario = [];
+        } else {
+            usuario = usuarios;
+        };
+        
+        for (let i = 0; i < usuarios.length; i++) {
+            if (req.body.email == usuario[i].email && bcrypt.compareSync(req.body.password, usuario[i].password)){
+                res.send('Te encontré');
+            }
+        }
+
+        res.send('error');
     },
     registed: (req, res) => {
         const nuevoUsuario = req.body;
-        const usuarioConImagen = {
-            ...nuevoUsuario,
+        const usuario = {
+            name: req.body.name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
             image: req.file.filename,
             id: new Date().getTime(),
         }
 
-        usuarios.push(usuarioConImagen);
+        usuarios.push(usuario);
         
         fs.writeFileSync(usersFilePath, JSON.stringify(usuarios));
 
