08f2ea26d39a65af858e026c20749b9cd9e06fd2
ramox-mq <daniel.rsi6oa@gmail.com>
2023-12-14 15:59:07 -0300

    agregue un boton

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mainController = require('../controllers/mainController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', mainController.index);
-router.get('/productCart', mainController.carrito);
+router.get('/productCart', authMiddleware, mainController.carrito);
 router.get('/quienesSomos', mainController.quienesSomos);
 
 // eliminar producto del carrito
