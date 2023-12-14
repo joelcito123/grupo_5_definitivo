595feacbfebe56731c6c5c85045d7d63e6a14fa6
ramox-mq <daniel.rsi6oa@gmail.com>
2023-12-14 15:35:30 -0300

    agregue el carrito de compras

--- src/routes/main.js
+++ src/routes/main.js
@@ -8,7 +8,13 @@ router.get('/productCart', mainController.carrito);
 router.get('/quienesSomos', mainController.quienesSomos);
 
 // eliminar producto del carrito
-router.delete('/eliminar/:id', mainController.eliminarDelCarrito)
+router.delete('/eliminar/:id', mainController.eliminarDelCarrito);
+
+//cantidad producto
+router.post('/cantidad/:productoId', mainController.cantidadProducto);
+
+//ordenar ya
+router.get('/ordenar-ya', mainController.ordenarYa)
 
 
 module.exports = router;
